60398c882dd48cb43eddc9bf4a3d03c19847c35e
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2016-03-24 23:01:47 +0100

    add auto-updated.sh wrapper script

---
+++ auto-updated.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+while [ true ]; do
+    ./fishnet.py "$@"
+    ret=$?
+    if [ $ret -eq 70 ]; then
+        for try in 1 2 3; do
+            pip install --upgrade fishnet
+            success=$?
+            if [ $success -eq 0 ]; then
+              break
+            else
+              sleep 10
+            fi
+        done
+
+        if [ $success -ne 0 ]; then
+          exit 70
+        fi
+    else
+        exit $ret
+    fi
+done
