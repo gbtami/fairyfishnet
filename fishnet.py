ea1584f2bb9927bf26a6f1c7215a8d185107d940
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-04-08 16:38:02 +0200

    bump 1.15.16

--- fishnet.py
+++ fishnet.py
@@ -94,7 +94,7 @@ except NameError:
     DEAD_ENGINE_ERRORS = (EOFError, IOError)
 
 
-__version__ = "1.15.15"
+__version__ = "1.15.16"
 
 __author__ = "Niklas Fiekas"
 __email__ = "niklas.fiekas@backscattering.de"
