f22a42b6314c4b2b98e44f9cad7f791ca27519b7
gbtami <gbtami@gmail.com>
2020-04-13 17:06:45 +0200

    Update requirements

--- fairyfishnet.py
+++ fairyfishnet.py
@@ -106,7 +106,7 @@ except NameError:
     DEAD_ENGINE_ERRORS = (EOFError, IOError)
 
 
-__version__ = "1.15.38"
+__version__ = "1.15.39"
 
 __author__ = "Bajusz Tamás"
 __email__ = "gbtami@gmail.com"
