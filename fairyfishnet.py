371aa2692f05cc18f50c2dec4905e21d6f8b210e
gbtami <gbtami@gmail.com>
2020-12-18 00:49:21 +0100

    Update requirements

--- fairyfishnet.py
+++ fairyfishnet.py
@@ -114,7 +114,7 @@ except NameError:
     DEAD_ENGINE_ERRORS = (EOFError, IOError)
 
 
-__version__ = "1.15.45"
+__version__ = "1.15.46"
 
 __author__ = "Bajusz Tamás"
 __email__ = "gbtami@gmail.com"
