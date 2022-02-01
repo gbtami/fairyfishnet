c4e36eae4b606f64af20fce343dfa247713179a8
Bajusz Tamás <gbtami@users.noreply.github.com>
2022-02-01 12:31:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Games/Entertainment :: Board Games",
         "Topic :: Internet :: WWW/HTTP",
     ]
