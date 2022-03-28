1061e39c8401f11d481e3ff0ea9941f0176360af
gbtami <gbtami@gmail.com>
2022-03-28 22:10:18 +0200

    Ordamirror nnue

--- fairyfishnet.py
+++ fairyfishnet.py
@@ -147,6 +147,7 @@ NNUE_NET = {
     "makruk": "e14ecd7ae145",
     "minishogi": "32efe2ac350b",
     "orda": "af8fab8f210b",
+    "ordamirror": "b432a42e3738",
     "seirawan": "d6ec9a356fa8",
     "shinobi": "4abe32d99161",
     "shogi": "878ca61334a7",
