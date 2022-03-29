be8dcb045c7903c7e650e2ae5d1532fccad6635c
gbtami <gbtami@gmail.com>
2022-03-29 13:18:56 +0200

    Sittuyin and Torishogi nnue

--- fairyfishnet.py
+++ fairyfishnet.py
@@ -152,7 +152,9 @@ NNUE_NET = {
     "shinobi": "4abe32d99161",
     "shogi": "878ca61334a7",
     "shogun": "04b4fe4bbad7",
+    "sittuyin": "3c3ed74dbe54",
     "synochess": "42aaabb0581f",
+    "torishogi": "0c3670e88a63",
     "xiangqi": "83f16c17fe26",
 }
 
