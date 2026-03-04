d57a588726fd9416f2613a4a6f6df0c84739eb22
gbtami <gbtami@gmail.com>
2026-03-04 10:57:49 +0100

    Bump version

--- fairyfishnet.py
+++ fairyfishnet.py
@@ -117,7 +117,7 @@ except NameError:
     DEAD_ENGINE_ERRORS = (EOFError, IOError)
 
 
-__version__ = "1.16.54"
+__version__ = "1.16.55"
 
 __author__ = "Bajusz Tamás"
 __email__ = "gbtami@gmail.com"
